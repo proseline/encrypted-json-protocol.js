eb27999e58ec84f245934de2481b82af8d941dc8
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-20 20:28:48 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,288 +1,288 @@
-var AJV = require("ajv");
-var Duplexify = require("duplexify");
-var assert = require("assert");
-var inherits = require("inherits");
-var lengthPrefixedStream = require("length-prefixed-stream");
-var sodium = require("sodium-universal");
-var through2 = require("through2");
+var AJV = require('ajv')
+var Duplexify = require('duplexify')
+var assert = require('assert')
+var inherits = require('inherits')
+var lengthPrefixedStream = require('length-prefixed-stream')
+var sodium = require('sodium-universal')
+var through2 = require('through2')
 
-var STREAM_NONCEBYTES = sodium.crypto_stream_NONCEBYTES;
-var STREAM_KEYBYTES = sodium.crypto_stream_KEYBYTES;
+var STREAM_NONCEBYTES = sodium.crypto_stream_NONCEBYTES
+var STREAM_KEYBYTES = sodium.crypto_stream_KEYBYTES
 
-var HANDSHAKE_PREFIX = 0;
+var HANDSHAKE_PREFIX = 0
 
-module.exports = function(options) {
-  assert.equal(typeof options, "object", "argument must be Object");
+module.exports = function (options) {
+  assert.strictEqual(typeof options, 'object', 'argument must be Object')
 
-  var version = options.version;
-  assert.equal(typeof version, "number", "version must be Number");
-  assert(version > 0, "version must be greater than zero");
-  assert(Number.isSafeInteger(version), "version must be safe integer");
+  var version = options.version
+  assert.strictEqual(typeof version, 'number', 'version must be Number')
+  assert(version > 0, 'version must be greater than zero')
+  assert(Number.isSafeInteger(version), 'version must be safe integer')
 
-  var messages = options.messages;
-  assert.equal(typeof messages, "object", "messages must be Object");
-  var messageNames = Object.keys(messages);
-  assert(messageNames.length !== 0, "messages must have properties");
+  var messages = options.messages
+  assert.strictEqual(typeof messages, 'object', 'messages must be Object')
+  var messageNames = Object.keys(messages)
+  assert(messageNames.length !== 0, 'messages must have properties')
 
-  var ajv = new AJV();
-  var messageTypesByName = {};
-  var messageTypesByPrefix = {};
-  var messageTypePrefixes = [0];
-  messageNames.sort().forEach(function(name, index) {
-    var options = messages[name];
-    assert(options.hasOwnProperty("schema"), "message type must have schema");
-    if (options.hasOwnProperty("verify")) {
-      assert.equal(
+  var ajv = new AJV()
+  var messageTypesByName = {}
+  var messageTypesByPrefix = {}
+  var messageTypePrefixes = [0]
+  messageNames.sort().forEach(function (name, index) {
+    var options = messages[name]
+    assert(options.hasOwnProperty('schema'), 'message type must have schema')
+    if (options.hasOwnProperty('verify')) {
+      assert.strictEqual(
         typeof options.verify,
-        "function",
-        "verify must be Function"
-      );
+        'function',
+        'verify must be Function'
+      )
     }
-    var schema = options.schema;
-    var valid = ajv.compile(schema);
-    var prefix = index + 1; // Reserve prefix 0 for handshakes.
-    messageTypePrefixes.push(prefix);
+    var schema = options.schema
+    var valid = ajv.compile(schema)
+    var prefix = index + 1 // Reserve prefix 0 for handshakes.
+    messageTypePrefixes.push(prefix)
     messageTypesByName[name] = messageTypesByPrefix[prefix] = {
       name: name,
       valid: valid,
       verify:
         options.verify ||
-        function() {
-          return true;
+        function () {
+          return true
         },
       prefix: prefix
-    };
-    Protocol.prototype[name] = function(data, callback) {
-      this._sendMessage(name, data, callback);
-    };
-  });
+    }
+    Protocol.prototype[name] = function (data, callback) {
+      this._sendMessage(name, data, callback)
+    }
+  })
 
   var validTuple = ajv.compile({
-    title: "Protocol Message",
-    type: "array",
+    title: 'Protocol Message',
+    type: 'array',
     items: [
       {
-        title: "Message Type Prefix",
-        type: "number",
+        title: 'Message Type Prefix',
+        type: 'number',
         enum: messageTypePrefixes
       },
       {
-        title: "Message Payload"
+        title: 'Message Payload'
         /* anything */
       }
     ],
     additionalItems: false
-  });
+  })
 
   var validHandshake = ajv.compile({
-    title: "Handshake Message",
-    type: "object",
+    title: 'Handshake Message',
+    type: 'object',
     properties: {
       version: {
-        title: "Protocol Version",
-        type: "number",
+        title: 'Protocol Version',
+        type: 'number',
         multipleOf: 1,
         minimum: 1
       },
       nonce: {
-        title: "Encryption Nonce",
-        type: "string",
-        pattern: "^[a-f0-9]{" + STREAM_NONCEBYTES * 2 + "}$"
+        title: 'Encryption Nonce',
+        type: 'string',
+        pattern: '^[a-f0-9]{' + STREAM_NONCEBYTES * 2 + '}$'
       }
     },
-    required: ["version", "nonce"],
+    required: ['version', 'nonce'],
     additionalProperties: false
-  });
+  })
 
-  function Protocol(options) {
-    assert.equal(typeof options, "object", "argument must be object");
+  function Protocol (options) {
+    assert.strictEqual(typeof options, 'object', 'argument must be object')
 
-    if (!(this instanceof Protocol)) return new Protocol(options);
+    if (!(this instanceof Protocol)) return new Protocol(options)
 
-    var replicationKey = (this._replicationKey = options.replicationKey);
-    assert(Buffer.isBuffer(replicationKey), "replicationKey must be Buffer");
-    assert.equal(
+    var replicationKey = (this._replicationKey = options.replicationKey)
+    assert(Buffer.isBuffer(replicationKey), 'replicationKey must be Buffer')
+    assert.strictEqual(
       replicationKey.byteLength,
       STREAM_KEYBYTES,
-      "replicationKey must be crypto_stream_KEYBYTES long"
-    );
+      'replicationKey must be crypto_stream_KEYBYTES long'
+    )
 
-    this._initializeReadable();
-    this._initializeWritable();
-    Duplexify.call(this, this._writableStream, this._readableStream);
+    this._initializeReadable()
+    this._initializeWritable()
+    Duplexify.call(this, this._writableStream, this._readableStream)
   }
 
-  Protocol.prototype._initializeReadable = function() {
-    var self = this;
+  Protocol.prototype._initializeReadable = function () {
+    var self = this
 
     // Cryptographic stream using our nonce and the secret key.
-    self._sendingNonce = Buffer.alloc(STREAM_NONCEBYTES);
-    sodium.randombytes_buf(self._sendingNonce);
+    self._sendingNonce = Buffer.alloc(STREAM_NONCEBYTES)
+    sodium.randombytes_buf(self._sendingNonce)
     self._sendingCipher = initializeCipher(
       self._sendingNonce,
       self._replicationKey
-    );
+    )
 
-    self._encoderStream = lengthPrefixedStream.encode();
+    self._encoderStream = lengthPrefixedStream.encode()
 
-    self._readableStream = through2.obj(function(chunk, _, done) {
-      assert(Buffer.isBuffer(chunk));
+    self._readableStream = through2.obj(function (chunk, _, done) {
+      assert(Buffer.isBuffer(chunk))
       // Once we've sent our nonce, encrypt.
       if (self._sentNonce) {
-        self._sendingCipher.update(chunk, chunk);
+        self._sendingCipher.update(chunk, chunk)
       }
-      this.push(chunk);
-      done();
-    });
+      this.push(chunk)
+      done()
+    })
 
     self._encoderStream
       .pipe(self._readableStream)
-      .once("error", function(error) {
-        self.destroy(error);
-      });
-  };
+      .once('error', function (error) {
+        self.destroy(error)
+      })
+  }
 
-  Protocol.prototype._initializeWritable = function() {
-    var self = this;
+  Protocol.prototype._initializeWritable = function () {
+    var self = this
 
     // Cryptographic stream using our peer's nonce, which we've yet
     // to receive, and the secret key.
-    self._receivingNonce = null;
-    self._receivingCipher = null;
+    self._receivingNonce = null
+    self._receivingCipher = null
 
-    self._writableStream = through2(function(chunk, encoding, done) {
-      assert(Buffer.isBuffer(chunk));
+    self._writableStream = through2(function (chunk, encoding, done) {
+      assert(Buffer.isBuffer(chunk))
       // Once we've been given a nonce, decrypt.
       if (self._receivingCipher) {
-        self._receivingCipher.update(chunk, chunk);
+        self._receivingCipher.update(chunk, chunk)
       }
       // Until we've been given a nonce, write in the clear.
-      done(null, chunk);
-    });
+      done(null, chunk)
+    })
 
-    self._parserStream = through2.obj(function(chunk, _, done) {
-      self._parse(chunk, function(error) {
-        if (error) return done(error);
-        done();
-      });
-    });
+    self._parserStream = through2.obj(function (chunk, _, done) {
+      self._parse(chunk, function (error) {
+        if (error) return done(error)
+        done()
+      })
+    })
 
     self._writableStream
       .pipe(lengthPrefixedStream.decode())
       .pipe(self._parserStream)
-      .once("error", function(error) {
-        self.destroy(error);
-      });
-  };
+      .once('error', function (error) {
+        self.destroy(error)
+      })
+  }
 
   // Send our handshake message.
-  Protocol.prototype.handshake = function(callback) {
-    assert.equal(typeof callback, "function");
-    var self = this;
-    if (self._sentNonce) return callback(new Error("already sent handshake"));
+  Protocol.prototype.handshake = function (callback) {
+    assert.strictEqual(typeof callback, 'function')
+    var self = this
+    if (self._sentNonce) return callback(new Error('already sent handshake'))
     self._encode(
       HANDSHAKE_PREFIX,
       {
         version: version,
-        nonce: self._sendingNonce.toString("hex")
+        nonce: self._sendingNonce.toString('hex')
       },
-      function(error) {
-        if (error) return callback(error);
-        self._sentNonce = true;
-        callback();
+      function (error) {
+        if (error) return callback(error)
+        self._sentNonce = true
+        callback()
       }
-    );
-  };
+    )
+  }
 
   // Send a protocol-defined message.
   //
   // The constructor adds functions to the prototype for sending each
   // message type, which call this function in turn.
-  Protocol.prototype._sendMessage = function(typeName, data, callback) {
+  Protocol.prototype._sendMessage = function (typeName, data, callback) {
     assert(
       messageTypesByName.hasOwnProperty(typeName),
-      "unknown message type: " + typeName
-    );
-    assert.equal(typeof callback, "function", "callback must be function");
-    var type = messageTypesByName[typeName];
+      'unknown message type: ' + typeName
+    )
+    assert.strictEqual(typeof callback, 'function', 'callback must be function')
+    var type = messageTypesByName[typeName]
     try {
-      assert(type.valid(data));
-      assert(type.verify(data));
+      assert(type.valid(data))
+      assert(type.verify(data))
     } catch (error) {
-      var moreInformativeError = new Error("invalid " + typeName);
-      moreInformativeError.errors = type.valid.errors;
-      throw moreInformativeError;
+      var moreInformativeError = new Error('invalid ' + typeName)
+      moreInformativeError.errors = type.valid.errors
+      throw moreInformativeError
     }
-    this._encode(type.prefix, data, callback);
-  };
+    this._encode(type.prefix, data, callback)
+  }
 
-  Protocol.prototype.finalize = function(callback) {
-    assert(typeof callback === "function");
-    var self = this;
-    self._finalize(function(error) {
-      if (error) return self.destroy(error);
-      self._encoderStream.end(callback);
-      self._sendingCipher.final();
-      self._sendingCipher = null;
-      self._receivingCipher.final();
-      self._receivingCipher = null;
-    });
-  };
+  Protocol.prototype.finalize = function (callback) {
+    assert(typeof callback === 'function')
+    var self = this
+    self._finalize(function (error) {
+      if (error) return self.destroy(error)
+      self._encoderStream.end(callback)
+      self._sendingCipher.final()
+      self._sendingCipher = null
+      self._receivingCipher.final()
+      self._receivingCipher = null
+    })
+  }
 
-  Protocol.prototype._encode = function(prefix, body, callback) {
-    var tuple = [prefix, body];
-    var buffer = Buffer.from(JSON.stringify(tuple), "utf8");
-    this._encoderStream.write(buffer, callback);
-  };
+  Protocol.prototype._encode = function (prefix, body, callback) {
+    var tuple = [prefix, body]
+    var buffer = Buffer.from(JSON.stringify(tuple), 'utf8')
+    this._encoderStream.write(buffer, callback)
+  }
 
-  Protocol.prototype._parse = function(message, callback) {
+  Protocol.prototype._parse = function (message, callback) {
     try {
-      var parsed = JSON.parse(message);
+      var parsed = JSON.parse(message)
     } catch (error) {
-      return callback(error);
+      return callback(error)
     }
     if (!validTuple(parsed)) {
-      return callback(new Error("invalid tuple"));
+      return callback(new Error('invalid tuple'))
     }
-    var prefix = parsed[0];
-    var body = parsed[1];
+    var prefix = parsed[0]
+    var body = parsed[1]
     if (prefix === 0 && validHandshake(body)) {
       if (version !== body.version) {
-        var error = new Error("version mismatch");
-        error.version = body.version;
-        return callback(error);
+        var error = new Error('version mismatch')
+        error.version = body.version
+        return callback(error)
       }
       if (!this._receivingCipher) {
-        this._receivingNonce = Buffer.from(body.nonce, "hex");
-        assert.equal(this._receivingNonce.byteLength, STREAM_NONCEBYTES);
+        this._receivingNonce = Buffer.from(body.nonce, 'hex')
+        assert.strictEqual(this._receivingNonce.byteLength, STREAM_NONCEBYTES)
         this._receivingCipher = initializeCipher(
           this._receivingNonce,
           this._replicationKey
-        );
-        this.emit("handshake");
-        return callback();
+        )
+        this.emit('handshake')
+        return callback()
       }
-      this.emit("handshake");
-      return callback();
+      this.emit('handshake')
+      return callback()
     }
-    var type = messageTypesByPrefix[prefix];
+    var type = messageTypesByPrefix[prefix]
     if (!type || !type.valid(body) || !type.verify(body)) {
-      this.emit("invalid", body);
-      return callback();
+      this.emit('invalid', body)
+      return callback()
     }
-    this.emit(type.name, body);
-    return callback();
-  };
+    this.emit(type.name, body)
+    return callback()
+  }
 
-  inherits(Protocol, Duplexify);
+  inherits(Protocol, Duplexify)
 
-  return Protocol;
-};
+  return Protocol
+}
 
-function initializeCipher(nonce, secretKey) {
-  assert(Buffer.isBuffer(nonce));
-  assert.equal(nonce.byteLength, STREAM_NONCEBYTES);
-  assert(Buffer.isBuffer(secretKey));
-  assert.equal(secretKey.byteLength, STREAM_KEYBYTES);
-  return sodium.crypto_stream_xor_instance(nonce, secretKey);
+function initializeCipher (nonce, secretKey) {
+  assert(Buffer.isBuffer(nonce))
+  assert.strictEqual(nonce.byteLength, STREAM_NONCEBYTES)
+  assert(Buffer.isBuffer(secretKey))
+  assert.strictEqual(secretKey.byteLength, STREAM_KEYBYTES)
+  return sodium.crypto_stream_xor_instance(nonce, secretKey)
 }
--- test.js
+++ test.js
@@ -1,175 +1,175 @@
-var makeProtocol = require("./");
-var sodium = require("sodium-universal");
-var tape = require("tape");
+var makeProtocol = require('./')
+var sodium = require('sodium-universal')
+var tape = require('tape')
 
 var FruitProtocol = makeProtocol({
   version: 1,
   messages: {
     apple: {
       schema: {
-        type: "string",
-        const: "apple"
+        type: 'string',
+        const: 'apple'
       }
     },
     orange: {
       schema: {
-        type: "string",
-        const: "orange"
+        type: 'string',
+        const: 'orange'
       }
     }
   }
-});
+})
 
-tape("apple and orange", function(test) {
-  test.plan(8);
+tape('apple and orange', function (test) {
+  test.plan(8)
 
-  var replicationKey = randomReplicationKey();
+  var replicationKey = randomReplicationKey()
 
-  var anna = FruitProtocol({ replicationKey });
-  anna.handshake(function(error) {
-    test.ifError(error, "anna sent handshake");
-  });
-  anna.once("handshake", function() {
-    test.pass("anna received handshake");
-    anna.once("apple", function(body) {
-      test.equal(body, "apple", "anna received apple");
-    });
-    anna.orange("orange", function(error) {
-      test.ifError(error, "anna sent orange");
-    });
-  });
+  var anna = FruitProtocol({ replicationKey })
+  anna.handshake(function (error) {
+    test.ifError(error, 'anna sent handshake')
+  })
+  anna.once('handshake', function () {
+    test.pass('anna received handshake')
+    anna.once('apple', function (body) {
+      test.equal(body, 'apple', 'anna received apple')
+    })
+    anna.orange('orange', function (error) {
+      test.ifError(error, 'anna sent orange')
+    })
+  })
 
-  var bob = FruitProtocol({ replicationKey });
-  bob.handshake(function(error) {
-    test.ifError(error, "bob sent handshake");
-  });
-  bob.once("handshake", function() {
-    test.pass("bob received handshake");
-    bob.once("orange", function(body) {
-      test.equal(body, "orange", "bob received orange");
-    });
-    bob.apple("apple", function(error) {
-      test.ifError(error, "bob sent apple");
-    });
-  });
+  var bob = FruitProtocol({ replicationKey })
+  bob.handshake(function (error) {
+    test.ifError(error, 'bob sent handshake')
+  })
+  bob.once('handshake', function () {
+    test.pass('bob received handshake')
+    bob.once('orange', function (body) {
+      test.equal(body, 'orange', 'bob received orange')
+    })
+    bob.apple('apple', function (error) {
+      test.ifError(error, 'bob sent apple')
+    })
+  })
 
-  anna.pipe(bob).pipe(anna);
-});
+  anna.pipe(bob).pipe(anna)
+})
 
-tape("multiple messages", function(test) {
-  test.plan(6);
+tape('multiple messages', function (test) {
+  test.plan(6)
 
-  var replicationKey = randomReplicationKey();
+  var replicationKey = randomReplicationKey()
 
-  var anna = FruitProtocol({ replicationKey });
-  anna.handshake(function(error) {
-    test.ifError(error, "anna sent handshake");
-    anna.on("apple", function() {
-      test.pass("anna received apple");
-    });
-    anna.on("orange", function() {
-      test.pass("anna received orange");
-    });
-  });
+  var anna = FruitProtocol({ replicationKey })
+  anna.handshake(function (error) {
+    test.ifError(error, 'anna sent handshake')
+    anna.on('apple', function () {
+      test.pass('anna received apple')
+    })
+    anna.on('orange', function () {
+      test.pass('anna received orange')
+    })
+  })
 
-  var bob = FruitProtocol({ replicationKey });
-  bob.handshake(function(error) {
-    test.ifError(error, "bob sent handshake");
-    bob.apple("apple", function(error) {
-      test.ifError(error, "bob sent handshake");
-    });
-    bob.orange("orange", function(error) {
-      test.ifError(error, "bob sent handshake");
-    });
-  });
+  var bob = FruitProtocol({ replicationKey })
+  bob.handshake(function (error) {
+    test.ifError(error, 'bob sent handshake')
+    bob.apple('apple', function (error) {
+      test.ifError(error, 'bob sent handshake')
+    })
+    bob.orange('orange', function (error) {
+      test.ifError(error, 'bob sent handshake')
+    })
+  })
 
-  anna.pipe(bob).pipe(anna);
-});
+  anna.pipe(bob).pipe(anna)
+})
 
-tape("version conflict", function(test) {
-  test.plan(6);
+tape('version conflict', function (test) {
+  test.plan(6)
 
   var Version1 = makeProtocol({
     version: 1,
-    messages: { hello: { schema: { type: "string", const: "hello" } } }
-  });
+    messages: { hello: { schema: { type: 'string', const: 'hello' } } }
+  })
 
   var Version2 = makeProtocol({
     version: 2,
-    messages: { howdy: { schema: { type: "string", const: "howdy" } } }
-  });
+    messages: { howdy: { schema: { type: 'string', const: 'howdy' } } }
+  })
 
-  var replicationKey = randomReplicationKey();
+  var replicationKey = randomReplicationKey()
 
-  var anna = Version1({ replicationKey }).once("error", function(error) {
-    test.equal(error.message, "version mismatch");
-    test.equal(error.version, 2);
-  });
-  anna.handshake(function(error) {
-    test.ifError(error, "anna sent handshake");
-  });
+  var anna = Version1({ replicationKey }).once('error', function (error) {
+    test.equal(error.message, 'version mismatch')
+    test.equal(error.version, 2)
+  })
+  anna.handshake(function (error) {
+    test.ifError(error, 'anna sent handshake')
+  })
 
-  var bob = Version2({ replicationKey }).once("error", function(error) {
-    test.equal(error.message, "version mismatch");
-    test.equal(error.version, 1);
-  });
-  bob.handshake(function(error) {
-    test.ifError(error, "anna sent handshake");
-  });
+  var bob = Version2({ replicationKey }).once('error', function (error) {
+    test.equal(error.message, 'version mismatch')
+    test.equal(error.version, 1)
+  })
+  bob.handshake(function (error) {
+    test.ifError(error, 'anna sent handshake')
+  })
 
-  anna.pipe(bob).pipe(anna);
-});
+  anna.pipe(bob).pipe(anna)
+})
 
-tape("double handshake", function(test) {
-  var replicationKey = randomReplicationKey();
-  var anna = FruitProtocol({ replicationKey });
-  anna.handshake(function(error) {
-    test.ifError(error);
-    anna.handshake(function(error) {
-      test.equal(error.message, "already sent handshake");
-      test.end();
-    });
-  });
-});
+tape('double handshake', function (test) {
+  var replicationKey = randomReplicationKey()
+  var anna = FruitProtocol({ replicationKey })
+  anna.handshake(function (error) {
+    test.ifError(error)
+    anna.handshake(function (error) {
+      test.equal(error.message, 'already sent handshake')
+      test.end()
+    })
+  })
+})
 
-tape("invalid message", function(test) {
-  var replicationKey = randomReplicationKey();
-  var anna = FruitProtocol({ replicationKey });
+tape('invalid message', function (test) {
+  var replicationKey = randomReplicationKey()
+  var anna = FruitProtocol({ replicationKey })
   test.throws(
-    function() {
-      anna.apple("orange", function() {
+    function () {
+      anna.apple('orange', function () {
         /* pass */
-      });
+      })
     },
-    "invalid apple",
-    "invalid apple"
-  );
-  test.end();
-});
+    'invalid apple',
+    'invalid apple'
+  )
+  test.end()
+})
 
-tape("verify", function(test) {
+tape('verify', function (test) {
   var ProtocolWithValid = makeProtocol({
     version: 1,
     messages: {
       hello: {
-        schema: { type: "string" },
-        verify: function(body) {
-          return body === "hello";
+        schema: { type: 'string' },
+        verify: function (body) {
+          return body === 'hello'
         }
       }
     }
-  });
-  var anna = ProtocolWithValid({ replicationKey: randomReplicationKey() });
-  test.throws(function() {
-    anna.hello("howdy", function() {
+  })
+  var anna = ProtocolWithValid({ replicationKey: randomReplicationKey() })
+  test.throws(function () {
+    anna.hello('howdy', function () {
       /* pass */
-    });
-  }, "invalid hello");
-  test.end();
-});
+    })
+  }, 'invalid hello')
+  test.end()
+})
 
-function randomReplicationKey() {
-  var key = Buffer.alloc(32);
-  sodium.randombytes_buf(key);
-  return key;
+function randomReplicationKey () {
+  var key = Buffer.alloc(32)
+  sodium.randombytes_buf(key)
+  return key
 }
